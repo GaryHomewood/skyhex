580c8d5bfc3195d886c67266a880e8bc14b9887e
Gary Homewood <gary.homewood@lbi.com>
2014-10-10 17:48:57 +0100

    picking dominant color from image

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,20 @@ SKYHEX = {
     updatePhoto: function() {
         var socket = io();
         socket.on('PHOTO_MOVED', function() {
-            $('#skyphoto').attr('src', '/images/img.png?' + Math.random());
-        });    
+            var src = '/images/img.png?' + Math.random();
+            $('#skyphoto').attr('src', src);
+
+            var image = new Image();
+            image.src = "/images/img.png";
+            var colorThief = new ColorThief();
+            var color = colorThief.getColor(image);
+
+            var r = parseInt(color[0], 10);
+            var g = parseInt(color[1], 10);
+            var b = parseInt(color[2], 10);
+            var hex = '#'+ r.toString(16) + g.toString(16) + b.toString(16);
+
+            $('body').css('background-color', hex);
+        });
     }
 }
