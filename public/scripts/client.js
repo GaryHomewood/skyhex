7fee5684b0f2c1b927fc5ee6edc77f3a63ae0625
Gary Homewood <gary.homewood@gmail.com>
2014-10-19 19:40:22 +0100

    adding dominant colour history

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,25 +3,37 @@ var SKYHEX = SKYHEX || {}
 SKYHEX = {
     updatePhoto: function() {
         var socket = io();
+        var src = "";
+
+        $('#skyphoto').load(function() {
+            // get the dominant colour once the image is loaded
+            var colorThief = new ColorThief();
+            var image = new Image();
+            image.src = src
+            var rgb = colorThief.getColor(image);
+
+            // convert the rgb to hex
+            var rgbCode = 'rgb( ' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')';
+            var hex = one.color(rgbCode).hex();
+
+            // show the current hex at the top
+            $('.colour').css('background-color', hex);
+            $('.colour p').text(hex);
+
+            // also add the current colour to the history
+            var historyitem = $("<div class='colour-history-item'><span>" + hex + "</span></div>");
+            historyitem.css('background-color', hex);
+            if ($('.colour-history-item').length) {
+                $('.colour-history-item').first().before(historyitem);
+            } else {
+                $('.colour-history').prepend(historyitem);
+            }
+        });
+
         socket.on('PHOTO_MOVED', function() {
-            var src = '/images/img.png?' + Math.random();
-            var img = $('#skyphoto');
-
-            img.load(function() {
-              // image loaded; now get the dominant colour
-              var image = new Image();
-              image.src = src
-              var colorThief = new ColorThief();
-              var color = colorThief.getColor(image);
-              var r = parseInt(color[0], 10);
-              var g = parseInt(color[1], 10);
-              var b = parseInt(color[2], 10);
-              var hex = '#'+ r.toString(16) + g.toString(16) + b.toString(16);
-              $('.colour').css('background-color', hex);
-              $('.colour p').text(hex);
-            });
-
-            img.attr('src', src);
+            // add a random querystring to force image refresh
+            src = '/images/img.png?' + Math.random();
+            $('#skyphoto').attr('src', src);
         });
     }
 }
--- server.js
+++ server.js
@@ -17,6 +17,9 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 io.on('connection', function(socket){
+    socket.on('take-photo', function(msg){
+      takePhoto();
+    });
 });
 
 function takePhoto() {
